8b124d94fb65f014992f8d82d8f64f8032330f51
Samir Djellil <samir@djellil.com>
2015-09-09 17:05:56 +0200

    Polish

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var gmatch = function(str, pattern) {
   var matches = [];
   while (match = pattern.exec(str)) {
-    var capturedGroups = extractCapturedGroups(match);
+    var capturedGroups = extractCapturingGroups(match);
     matches.push(capturedGroups);
   }
   return matches;
 }
 
-var extractCapturedGroups = function(match) {
+var extractCapturingGroups = function(match) {
   var capturedGroups = [];
   for (var i = 1; i < match.length; i++) {
     capturedGroups.push(match[i]);
